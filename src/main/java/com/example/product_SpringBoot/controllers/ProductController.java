0762599866e3054034f59386cf8f75077a8a22c5
barbaraleles <barbara_rlf@hotmail.com>
2025-05-29 13:17:57 -0300

    atualizacao requisicoes get, put, delete

--- src/main/java/com/example/product_SpringBoot/controllers/ProductController.java
+++ src/main/java/com/example/product_SpringBoot/controllers/ProductController.java
@@ -6,11 +6,14 @@ import com.example.product_SpringBoot.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -19,10 +22,53 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    //O @valid tem que ser inserido para que as validacoes do record funcionem
+    //para incluir tem que passar todo o body por isso o requestbody
     @PostMapping("/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
         var productModel = new ProductModel();
+        //conversao do dto para model
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
+
+    @GetMapping("/products")
+    public ResponseEntity<List> getAllProduct(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> product = productRepository.findById(id);
+        if (product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
+    }
+
+    //assim como para incluir o produto é inserido o body para atualizar tambem e necessario recebe-lo
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody @Valid ProductRecordDto productRecordDto){
+        Optional<ProductModel> product = productRepository.findById(id);
+        if (product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        //utiliza o product.get() para que o id seja o mesmo, pq se instanciar um model do zero gera outro id
+        var productModel = product.get();
+        //conversao do dto para model
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> product = productRepository.findById(id);
+        if (product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        productRepository.delete(product.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted with success");
+    }
+
 }
